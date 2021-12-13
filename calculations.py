18ea057a36eabcbe97f257ebb4e237004f35158e
ro-34 <87031449+ro-34@users.noreply.github.com>
2021-12-13 14:08:24 -0500

    Add files via upload

---
+++ calculations.py
@@ -0,0 +1,135 @@
+"""CSC110 Fall 2021 Project, Calculations
+
+Description
+===============================
+The calculations file reads and transforms data from the dataset, forming a complete list of Industry objects.
+It computes all the rates and potential scores seen in the Industry dataclass.
+
+Assumptions
+===============================
+We made a few assumptions (that are also mentioned below):
+- COVID-19 restrictions are completely over starting 2023
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the use of TAs
+marking CSC110 projects at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC110 materials,
+please consult us at our email addresses.
+
+This file is Copyright (c) 2021 Juhwan Son, Defne Altiok, Aliyah James, and Rohma Daud.
+"""
+
+import csv
+import helpers
+from helpers import JobMarket
+
+# Global variables -- NOT THE BEST PRACTICE, SOMEHOW FIND BETTER WAY
+# A JobMarket object that holds the results of all of our computations
+CANADA = JobMarket('Canada', helpers.Rates([], [], []), [])
+
+# Unmployment rate calculations
+def average_unemployment_rate(rates: list[float]) -> float:
+    """ Return the average unemployment rate based on a list of rates.
+
+    Preconditions:
+        - {type(rate) == type(0.4) for rate in rates}
+
+     >>> average_unemployment_rate()
+
+    """
+    return sum(rates) / len(rates)
+
+
+def rates_without_COVID() -> list[float]:
+    """
+    f
+    """
+
+    return []
+
+
+def predicted_rates() -> list[float]:
+    """
+    abc
+    """
+
+    return []
+
+
+# Calculations related to factors that affect unemployment
+def calculate_COVID_impact() -> int:
+    """
+    f
+    """
+
+    return 0
+
+
+def calculate_remote_work_potential() -> int:
+    """
+    d
+    """
+
+    return 0
+
+
+def calculate_recovery_potential() -> int:
+    """
+    d
+    """
+
+    return 0
+
+
+# Reading from datasets
+def read_data(filenames: list) -> JobMarket:
+    """"""
+    country_data = read_national_data(filenames[0])
+    industries = read_industry_data(filenames[1])
+    return JobMarket('Canada', country_data, industries)
+
+def read_national_data(filename: str) -> helpers.Rates:
+    """Return the data stored in the given file.
+
+    The file is a CSV file with two columns, one columns gives the month and year while the other gives the unemployment
+    rate. These files are based on real  data from the Canadian Government.
+    """
+    # ACCUMULATOR data_output: The Unemployment parsed from file so far
+    data_output = []
+    with open(filename) as file:
+        reader = csv.reader(file)
+
+        # Skip header row
+        next(reader)
+        data = [row for row in reader if len(row) == 2]  # Length of each row is 2 ; Skips last row
+        # for row in data:
+        # data_output += [Unemployment(row[0], float(row[1]))]
+        for row in data:
+            row[1] = float(row[1])
+    # return data
+    return helpers.Rates([], [], [])
+
+
+def read_industry_data(filename: str) -> list[helpers.Industry]:
+    """Return the data stored in the given file.
+
+    The file is a CSV file with two columns, one columns gives the month and year while the other gives the unemployment
+    rate. These files are based on real  data from the Canadian Government.
+    """
+    # ACCUMULATOR data_output: The Unemployment parsed from file so far
+    data_output = []
+    with open(filename) as file:
+        reader = csv.reader(file)
+
+        # Skip header row
+        next(reader)
+        data = [row for row in reader if len(row) == 2]  # Length of each row is 2 ; Skips last row
+        # for row in data:
+        # data_output += [Unemployment(row[0], float(row[1]))]
+        for row in data:
+            row[1] = float(row[1])
+    #return data
+    return []
---
+++ helpers.py
@@ -0,0 +1,196 @@
+"""CSC110 Fall 2021 Project, Helper Module
+
+Description
+===============================
+The helper module stores several constants, functions and data classes used in our calculations related to the
+unemployment rates of the different industries in Canada.
+
+Short-forms:
+ur = unemployment rate
+rps = recovery potential score
+rwps = remote work potential score
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the use of TAs
+marking CSC110 projects at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC110 materials,
+please consult us at our email addresses.
+
+This file is Copyright (c) 2021 Juhwan Son, Defne Altiok, Aliyah James, and Rohma Daud.
+"""
+
+from dataclasses import dataclass
+
+# Global variables
+# Names of all Canadian industries we'll be tracking
+INDUSTRIES = [
+    'Accommodation and food services',
+    'Information, culture and recreation',
+    'Forestry, fishing, mining, quarrying, oil and gas',
+    'Business, building and other support services',
+    'Construction',
+    'Other services (except public administration)',
+    'Transportation and warehousing',
+    'Wholesale and retail trade',
+    'Manufacturing',
+    'Educational services',
+    'Agriculture',
+    'Professional, scientific and technical services',
+    'Health care and social assistance',
+    'Finance, insurance, real estate, rental and leasing',
+    'Public administration',
+    'Utilities'
+]
+
+# Classes
+@dataclass
+class Factors:
+    """A bundle of possible factors that affect the potential of a job industry
+    to be able to work remotely and/or recover from the lockdowns and restrictions
+     that occurred due to the pandemic.
+
+    Instance Attributes:
+        -
+
+    Representation Invariants:
+        -
+    """
+    physical_proximity: int
+    essential: bool
+    front_line: bool
+
+
+
+@dataclass
+class Rates:
+    """A bundle of all the unemployment rates to display for a job industry.
+
+    Instance Attributes:
+        - employment_rates: a list of the unemployment rates for this industry, from 2016 to 2021
+        - predicted_rates: a list of the predicted unemployment rates for this industry, from 2022 to 2024
+        - rates_without_COVID: a list of the unemployment rates if COVID hadn't happened, from 2020 to 2024
+
+    """
+    unemployment_rates: list[float]
+    predicted_rates: list[float]
+    rates_without_COVID: list[float]
+
+
+@dataclass
+class Industry:
+    """A job industry in the Canadian job market.
+
+    Instance Attributes:
+        - name: an index that points to a name in the INDUSTRIES list
+        - rates: the unemployment rates (current and future) for this job industry
+        - factors: the factors that affect this industry's ability to succeed during/after COVID-19
+        - impact: a score that measures how much this industry was impacted by COVID, on a scale of 1-5
+        - potential_remote_work: a score that measures this industry's potential to work remotely
+        - recovery_potential: a score that measures how likely this industry is to recover after COVID-19
+
+    Representation Invariants:
+        - 0 <= self.name < len(INDUSTRIES)
+        - len(employee_numbers) == 6
+        - 0 <= potential_remote_work <= 100
+        - 0 <= recovery_potential <= 100
+
+    """
+    name: int
+    rates: Rates
+    factors: Factors
+    impact: int
+    potential_remote_work: int
+    recovery_potential: int
+
+
+class JobMarket:
+    """A class that holds all the data related to unemployment rates
+     in a country's job market.
+
+    Instance Attributes:
+        - country_name: the  name of the country whose job market this is
+        - rates: the unemployment rates (current and future) for the country
+        - industries: a list of all the job industries in this country
+
+    Representation Invariants:
+        -
+
+    """
+    country_name: str
+    rates: Rates
+    industries: list[Industry]
+
+    def __init__(self, name: str, rates: Rates, industries: list[Industry]) -> None:
+        """Initialize a new country's job market/unemployment statistics object
+        with the name of the country.
+        """
+        self.country_name = name
+
+        self.rates = rates
+        self.industries = industries
+
+    def get_industry(self, name: str) -> Industry:
+        """Return the Industry object corresponding to the name passed in.
+        """
+        index = name_to_int(name)
+
+        for industry in self.industries:
+            if index == industry.name:
+                return industry
+
+    def rates_range(self) -> list[float]:
+        """"""
+        return []
+
+
+# Helper functions
+def year_to_index(year: int) -> int:
+    """Returns the corresponding list position of a specific year. For example, a list of
+     total workers in the Forestry, fishing, mining, quarrying, oil and gas industry
+     could be [20000, 30000, 43100, 24500, 43000]. The second element corresponds
+     to the total number of workers in 2017.
+
+     Preconditions:
+     - year in [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+
+     >>> year_to_index(2017)
+     1
+    """
+    years = [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+
+    for i in range(0, len(years)):
+        if years[i] == year:
+            return i
+
+
+def int_to_name(name: int) -> str:
+    """ Return the name of this industry using the index stored in self.name
+    and the list of industry names in INDUSTRIES.
+
+    Preconditions:
+    - 0 <= self.name < len(INDUSTRIES)
+
+    >>> industry = Industry(2, Rates([], [], []), Factors(0), 0, 0)
+    >>> int_to_name(industry.name)
+    Forestry, fishing, mining, quarrying, oil and gas
+    """
+    return INDUSTRIES[name]
+
+
+def name_to_int(name: str) -> int:
+    """ Return the index of this industry in INDUSTRIES using the name passed in.
+
+        Preconditions:
+        - name in INDUSTRIES
+
+        >>> industry = Industry(2, Rates([], [], []), Factors(0), 0, 0)
+        >>> name_to_int('Forestry, fishing, mining, quarrying, oil and gas')
+        2
+        """
+
+    for i in range(0, len(INDUSTRIES)):
+        if name == INDUSTRIES[i]:
+            return i
